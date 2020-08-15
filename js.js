1f65311f24bc35a40bf5e30d8cfb2f29e64542a3
MAC <mac@MACde-iMac.local>
2020-08-15 08:01:52 +0800

    cars and shots

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
 function show() {
-    
+    document.getElementById("lyrics").style.textShadow = "-0.05em 0em red, 0.05em 0em red, 0em -0.05em red, 0em 0.05em red";
+    document.getElementById("lyrics").innerHTML = "樁";
 }
