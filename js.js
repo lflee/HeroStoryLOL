ad9125cb25c3b042b2161bb5428838b028f21546
MAC <mac@MACde-iMac.local>
2020-08-20 19:46:22 +0800

    Added verbs

--- js.js
+++ js.js
@@ -1,4 +1,6 @@
 function show() {
-    document.getElementById("lyrics").style.textShadow = "-0.05em 0em red, 0.05em 0em red, 0em -0.05em red, 0em 0.05em red";
-    document.getElementById("lyrics").innerHTML = "樁";
+    document.getElementById("lyrics").style.textShadow = "-0.01em 0em black, 0.01em 0em black, 0em -0.01em black, 0em 0.01em black, -0.03em 0em red, 0.03em 0em red, 0em -0.03em red, 0em 0.03em red, -0.06em 0em blue, 0.06em 0em blue, 0em -0.06em blue, 0em 0.06em blue, -0.08em 0em yellow, 0.08em 0em yellow, 0em -0.08em yellow, 0em 0.08em yellow";
+
+
+    document.getElementById("lyrics").innerHTML = "樁鳩你";
 }
